e2dad05c14e8b36d3da64d9affbc958d762d15ea
cherubina-gabriak <a.naumovich2017@yandex.ru>
2022-04-02 13:28:27 +0300

    deleted some files

--- src/components/Form.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Form() {
-  return (
-    <form action="" >
-        <input type="text" placeholder="Enter a country" className="color_input" />
-    </form>
-  )
-}
