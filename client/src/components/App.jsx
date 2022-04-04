18298f3f1086e34f20f86417e09e49c68bdbb84a
cherubina-gabriak <a.naumovich2017@yandex.ru>
2022-04-04 16:51:00 +0300

    added playlists display

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import TrackSlider from './TrackSlider';
 import Favorites from './Favorites';
 
@@ -8,17 +8,26 @@ import world from '@svg-maps/world'
 import { SVGMap } from 'react-svg-map'
 import AppHeader from './AppHeader';
 import { getRequestCountry } from './../store/asuncActions/index';
-
+import { getSearch } from '../api/index'
 
 function App() {
-  const [isFavorites, setIsFavorites] = useState(false)
+  const [isFavorites, setIsFavorites] = useState(false);
   const [isSelected, setSelected] = useState(false);
+  const [playlists, setPlaylists] = useState([]);
+  const country = useSelector((state) => state.country.en);
   const dispatch = useDispatch()
 
-  function handleClick(e) {
-    setSelected(true)
-    dispatch(getRequestCountry(e.target.id))
+  async function handleClick(e) {
+    setSelected(true);
+    dispatch(getRequestCountry(e.target.id)); 
   }
+
+  useEffect(() => {
+     function getSearchResult() {
+      getSearch(country).then(res => setPlaylists(res));
+    }
+    getSearchResult();
+  }, [country])
 
   return (
     <div className="App">
@@ -28,7 +37,7 @@ function App() {
         <SVGMap className='map' map={world} onLocationClick={handleClick} />
         <TrackSlider />
       </div>
-      {isSelected && <PlaylistModal />}
+      {isSelected && <PlaylistModal playlists={playlists}/>}
     </div>
   );
 };
