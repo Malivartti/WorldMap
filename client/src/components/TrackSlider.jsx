4a50ef26dd50113830ab961f11999418e78c6b64
Dima <61251772+DimaSU2020@users.noreply.github.com>
2022-04-13 17:13:02 +0300

    change fonts

--- client/src/components/TrackSlider.jsx
+++ client/src/components/TrackSlider.jsx
@@ -111,7 +111,7 @@ export default function TrackSlider() {
           <img className='track-slider__info-cover' src={getImageUrl(trackData)} alt='' />
           <div className='track-slider__info-meta'>
             <h3 className='track-slider__info-title'>{trackData.name}</h3>
-            <span className='track-slider__info-auth'>{trackData.author.name} | <b>{trackData.country}</b> </span>
+            <span className='track-slider__info-auth'><b>{trackData.author.name}</b> | {trackData.country} </span>
           </div>
           <div className='track-slider__info-btns'>
             <button className='track-slider__info-btn' onClick={() => setIsShow(!isShow)}><VidoBtn fill={isShow ? '#0075ff' : ''} /></button>
